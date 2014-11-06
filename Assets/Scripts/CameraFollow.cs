82d3907075d9c8f17304c4c196f6756a1344af01
Zafiku <diegotorresortiz@hotmail.es>
2014-11-06 19:12:55 +0100

    Tarea 03 - Seguimiento de cámara
    Script mov. cámara con el personaje.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target; 
+	public float damping = 1;
+	public float lookAheadFactor = 3;
+	public float lookAheadReturnSpeed = 0.5f;
+	public float lookAheadMoveThreshold = 0.1f;
+	public float yPosRestiction = -1;
+	
+	float offsetZ;
+	Vector3 lastTargetPosition;
+	Vector3 currentVelocity;
+	Vector3 lookAheadPos;
+
+	void Start () {
+		lastTargetPosition = target.position;
+		offsetZ = (transform.position - target.position).z;
+		transform.parent = null;
+	}
+
+	void Update () {
+
+		float xMoveDelta = (target.position - lastTargetPosition).x;
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+		
+		if (updateLookAheadTarget) {
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+		} else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		
+		newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y,yPosRestiction,Mathf.Infinity), newPos.z);
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;		
+	}
+}
